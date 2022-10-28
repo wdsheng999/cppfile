8fefb8f33fb54cfdde9772d1fd25efa569bf9b0d
wwwds <wdsheng999@qq.com>
2022-10-28 17:34:28 +0800

    assgn0

---
+++ cs106b/StanfordCPPLib/StanfordCPPLib/util/sound.cpp
@@ -0,0 +1,86 @@
+/*
+ * File: sound.cpp
+ * ---------------
+ * Implementation of the Sound class.
+ * 
+ * @version 2015/07/05
+ * - removed static global Platform variable, replaced by getPlatform as needed
+ * @version 2014/10/08
+ * - removed 'using namespace' statement
+ * - removed unused include statements
+ */
+
+#define INTERNAL_INCLUDE 1
+#include "sound.h"
+#include <QUrl>
+#define INTERNAL_INCLUDE 1
+#include "filelib.h"
+#define INTERNAL_INCLUDE 1
+#include "gthread.h"
+#define INTERNAL_INCLUDE 1
+#include "require.h"
+#undef INTERNAL_INCLUDE
+
+/*static*/ QMediaPlayer* Sound::_qmediaPlayer = nullptr;
+
+/*static*/ long Sound::getDuration() {
+    initialize();
+    return _qmediaPlayer->duration();
+}
+
+/*static*/ int Sound::getVolume() {
+    initialize();
+    return _qmediaPlayer->volume();
+}
+
+/*static*/ void Sound::initialize() {
+    if (!_qmediaPlayer) {
+        GThread::runOnQtGuiThread([]() {
+            if (!_qmediaPlayer) {
+                _qmediaPlayer = new QMediaPlayer;
+            }
+        });
+    }
+}
+
+/*static*/ void Sound::pause() {
+    initialize();
+    _qmediaPlayer->pause();
+}
+
+/*static*/ void Sound::playSound(const std::string& filename) {
+    initialize();
+    std::string absPath = getAbsolutePath(filename);
+    if (!fileExists(absPath)) {
+        error("Sound::playSound: file not found: " + filename);
+    }
+
+    GThread::runOnQtGuiThreadAsync([absPath]() {
+        _qmediaPlayer->setMedia(QUrl::fromLocalFile(QString::fromStdString(absPath)));
+        _qmediaPlayer->play();
+    });
+}
+
+/*static*/ void Sound::stop() {
+    initialize();
+    _qmediaPlayer->stop();
+}
+
+/*static*/ void Sound::setVolume(int volume) {
+    initialize();
+    require::inRange(volume, 0, 100, "Sound::setVolume", "volume");
+    _qmediaPlayer->setVolume(volume);
+}
+
+Sound::Sound(std::string filename)
+        : _filename(filename) {
+    initialize();
+}
+
+Sound::~Sound() {
+    // TODO
+}
+
+void Sound::play() {
+    playSound(_filename);
+}
--- cs106b/readme.md
+++ cs106b/readme.md
@@ -5,7 +5,14 @@ install Qt
  check install --> sample-project/pro文件
 检查通过
 编写gitignore文件来忽略编译产生的文件
----
+
+**error log**
+项目中用到了java 直接编译没问题 但是不运行
+在这个网站 找到了问题 (https://web.stanford.edu/class/archive/cs/cs106b/cs106b.1174/resources/qt-creator/qt-creator-troubleshooting.html)
+>Q: If I create a C++ project of my own, it builds and runs fine. But when I try the SampleProject or the Homework ZIP project, it builds successfully and then doesn't run. Why not?
+A: This usually means you don't have Java JDK 7 properly installed as described above. Follow the Java steps above to ensure that you do have Java.
+
+QA文档保存在cs106b/troubleshooting
 
 follow https://github.com/Jack-Chan-2001/CS106B
 2018 和b站课程统一
