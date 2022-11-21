da31b827ceb3189f34866de050b426f638ce2122
wwwds <wdsheng999@qq.com>
2022-11-21 23:29:38 +0800

    setting up gramMap

--- cs106b/assign2/hw2 serafini.md
+++ cs106b/assign2/hw2 serafini.md
@@ -69,3 +69,19 @@ Another subtle issue is that you do not reuse words that have been included in a
 内存资源占用多 用小字典
 
 ## PartB Random writter
+
+you will write a program that reads an input file and uses it to build a large data structure of word groups called "N- grams" as a basis for randomly generating new text that sounds like it came from the same author as that file.
+
+using:Map / Vector 
+
+### step1 building map of Ngram
+
+into steps:
+1 welcome
+2 read input:Unlike in the previous assignment, here we are interested in each word. If you want to read a file one word at a time, an effective way to do so is using the input >> variable; syntax rather than getLine, etc.
+
+
+
+### step2 generate random text
+Each particular piece of text randomly generated in this way is also called a Markov chain. Markov chains are very useful in computer science and elsewhere, such as artificial intelligence, machine learning, economics, and statistics.
+
--- cs106b/assign2/serafini_starter/Ngrams/src/ngrams.cpp
+++ cs106b/assign2/serafini_starter/Ngrams/src/ngrams.cpp
@@ -8,13 +8,71 @@
 #include <iostream>
 #include <string>
 #include "console.h"
+#include"map.h"
+#include"vector.h"
+#include"simpio.h"
+#include"strlib.h"
 
 using namespace std;
-
+void welcome();
+void getInputFile(fstream& input,string& filename, int gramN);
+Map<Vector<string>,Vector<string>> generateNgram(const int gramN,fstream& input);
 int main() {
     // TODO: Finish the program!
+    fstream input;//input
+    string filename;
+    int gramN=0;
+    Map<Vector<string>,Vector<string>> ngramMap;
+    welcome();
+    getInputFile(input,filename,gramN);
+    ngramMap= generateNgram(gramN,input);
+    cout<<ngramMap;
 
 
     cout << "Exiting." << endl;
     return 0;
+}
+void welcome(){
+    cout<<"Welcome to CS 106B Random Writer ('N-Grams').\n\
+    This program makes random text based on a document. \n\
+    Give me an input file and an 'N' value for groups \n\
+    of words, and I'll create random text for you."<<endl;
+}
+
+void getInputFile(fstream& input,string& filename, int& gramN){
+
+    do{
+        filename=getLine("Input file name?");
+        input.open(filename);
+        if(!input.is_open()){
+            cout<<"Unable to open that file.  Try again."<<endl;
+        }
+    }while(!input.is_open());
+
+    do{
+        gramN=getInteger("Value of N?");
+        if(gramN<2){
+            cout<<"N must be 2 or greater."<<endl;
+        }
+
+    }while(gramN<2);
+
+}
+Map<Vector<string>,Vector<string>> generateNgram(const int gramN,fstream& input){
+    Map<Vector<string>,Vector<string>> ngramMap;
+    string word;
+    Vector<string> wordlist;
+    Vector<string> window,suffix;
+    while(input>>word){
+        wordlist.add(word);
+    }
+    for(int i=0;(i=wordlist.size());i++){
+        window.clear();
+        for(int j=i;j<i+gramN-1;j++){
+            window.add(wordlist[j%wordlist.size()]);
+        }
+        suffix.add(wordlist.get(i+gramN));
+        ngramMap.put(window,suffix);
+    }
+    return ngramMap;
 }
